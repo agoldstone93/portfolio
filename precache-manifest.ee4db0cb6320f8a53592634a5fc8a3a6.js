2654cc42def6b77aead9b08c065057bc06afb782
Adam Goldstone <amg93@msn.com>
2020-06-28 17:25:50 +0100

    Updates

---
+++ precache-manifest.ee4db0cb6320f8a53592634a5fc8a3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b275ef881373281fd91139ace2e57891",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9eb05f30cc768b5b2932",
+    "url": "/portfolio/static/css/main.a09e4a26.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb05f30cc768b5b2932",
+    "url": "/portfolio/static/js/main.a6d3a214.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
