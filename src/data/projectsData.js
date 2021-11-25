e8372fb611f00682cb3da03ae4fab81a759e6b2b
Adam Goldstone <amg93@msn.com>
2021-11-25 23:11:28 +0000

    tidying up data and adding tags to some

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,42 +3,52 @@ export default [
 		id: 5,
 		name: "Markdown Previewer",
 		description:
-			"I created a web app which allows a user to type in plain text. The app will convert it into markdown styled text.",
-		url: "https://codepen.io/amg93/full/MWKpJME"
+			`I created a web app which allows a user to type in plain text. The app will convert 
+			it into markdown styled text.`,
+		url: "https://codepen.io/amg93/full/MWKpJME",
+		tags: ['Markdown']
 	},
 	{
 		id: 4,
 		name: "Random Quote Generator",
 		description:
-			"This is a web app which will generate a random quote from an API. The user can click a button to generate a new quote and another button to tweet the quote and its author.",
-		url: "https://codepen.io/amg93/full/NWxpNNB"
+			`This is a web app which will generate a random quote from an API. The user can click 
+			a button to generate a new quote and another button to tweet the quote and its author.`,
+		url: "https://codepen.io/amg93/full/NWxpNNB",
+		tags: ['API']
 	},
 	{
 		id: 6,
 		name: "Palindrome Checker",
 		description:
-			"I built a simple JavaScript program to check if a string is a palindrome.",
-		url: "https://repl.it/@agoldstone93/PalindromeChecker"
+			`I built a simple JavaScript program to check if a string is a palindrome.`,
+		url: "https://repl.it/@agoldstone93/PalindromeChecker",
+		tags: ['Javascript']
 	},
 	{
 		id: 3,
 		name: "Technical Documentation Page",
 		description:
-			"This is a page which displays a lot of information within a technical document.",
-		url: "https://codepen.io/amg93/full/KKpJzYZ"
+			`This is a page which displays a lot of information within a technical document.`,
+		url: "https://codepen.io/amg93/full/KKpJzYZ",
+		tags: ['HTML', 'CSS']
+
 	},
 	{
 		id: 2,
 		name: "Product Landing Page",
 		description:
-			"This is an example product landing page. It contains an embedded video and information laid out clearly.",
-		url: "https://codepen.io/amg93/full/abOXzQL"
+			`This is an example product landing page. It contains an embedded video and information 
+			laid out clearly.`,
+		url: "https://codepen.io/amg93/full/abOXzQL",
+		tags: []
 	},
 	{
 		id: 1,
 		name: "Survey Form",
 		description:
-			"This is a basic survey form which allows users to enter information about themselves.",
-		url: "https://codepen.io/amg93/full/qBdEQNJ"
+			`This is a basic survey form which allows users to enter information about themselves.`,
+		url: "https://codepen.io/amg93/full/qBdEQNJ",
+		tags: []
 	}
 ];
