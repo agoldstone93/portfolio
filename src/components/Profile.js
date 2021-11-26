7189e5ac15bd94ddb9112f2fd925a5005ef4ee3e
Adam Goldstone <amg93@msn.com>
2021-11-26 15:39:28 +0000

    updated profile text

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Profile() {
     return (
         <p>
-            I am a self taught programmer with experience using HTML, CSS, JavaScript,
-            React and C.
+            I am a Junior Software Developer with experience using HTML, CSS, JavaScript,
+            React, C# and .NET. I have experience using Git and am familiar with techniques
+            such as Test Driven Development.
         </p>
     )
 }
