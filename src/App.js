f3f27383726ebaf6e6e2115695f084c412b2c85d
Adam Goldstone <amg93@msn.com>
2022-10-23 10:32:14 +0100

    update to react 18

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 import './App.css';
 
 import Header from './components/Header';
@@ -65,4 +65,6 @@ export default function App() {
 	);
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
